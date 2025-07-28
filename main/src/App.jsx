2dd4d965e7c450c5a5e46fa60f1845fc01f0c09f
Pkpkawe <kawepietro50@gmail.com>
2025-07-28 13:14:10 -0300

    feat: Criação da estrutura de rotas
    Organização da estrutura de rotas foi feita. Para isso, foi instalado a biblioteca react-router-dom.

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,10 +1,20 @@
 // Import React
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Import Pages
+
 
 // Function
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<></>} /> {/* Home */}
+        <Route path="/Library" element={<></>} /> {/* Library */}
+        <Route path="/About" element={<></>} /> {/* About */}
+      </Routes>
+    </Router>
   )
 }
 
